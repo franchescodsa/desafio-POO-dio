c239ee26aced2155aad71f80f27883053e0ceef0
franchescodsa <franchescodsa@gmail.com>
2022-11-21 09:48:16 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+Curso curso1=new Curso("Curso java","descrição curso java",8);
+Curso curso2=new Curso("Curso js","descrição curso js",4);
+
+Mentoria mentoria=new Mentoria("mentoria java","descrição de mentoria", LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
